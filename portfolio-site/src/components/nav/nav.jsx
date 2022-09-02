490a87e58f0b17e47190b5c331b9e2bf6f60553d
Samudra Perera <samudrapup@gmail.com>
2022-09-02 00:37:41 -0400

    Completed Styling, need media queries and menu hamburger functionality

--- portfolio-site/src/components/nav/nav.jsx
+++ portfolio-site/src/components/nav/nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './nav.css'
-//import myLogo from '../'
+import Logo from '../../img/logo.png'
 
 const Nav = () => {
   return (
     <div className='navContainer'>
         <div className='nav-left'>
             <div className='nav-left wrapper'>
-                <a className='nav-left img' href='/'>
-                    <img src='' alt='SamudraLogo'/>
-                    <span className='nameTitle'>Samudra Perera</span>
-                </a>
+                <div className='nav-left-home'>
+                    <a className='nav-left-img' href='/'>
+                        <img className='logoTitile' src={Logo} alt='SamudraLogo'/>
+                        <span className='nameTitle'>Samudra Perera</span>
+                    </a>
+                </div>
                 <div className='nav-left-social-links'>
                     <a className='linkedIn' href='https://www.linkedin.com/in/samudraperera/' target='_blank' rel='noreferrer'>
                         <i class="fa-brands fa-linkedin"></i>
@@ -25,15 +27,15 @@ const Nav = () => {
                         <i class="fa-brands fa-goodreads"></i>
                     </a>
                 </div>
-
             </div>
         </div>
         <div className='nav-right'>
             <div className='nav-right wrapper'>
-                <a className='about-me' href='#about-me'>About Me</a>
-                <a className='projects' href='#projects'>Propjects</a>
-                <a className='resume' href='#resume'>Resume</a>
-
+                <div className='nav-right-anchor-container'>
+                    <a className='about-me' href='#about-me'>About Me</a>
+                    <a className='projects' href='#projects'>Projects</a>
+                    <a className='resume' href='https://www.canva.com/design/DAFHfQnm6Y4/EjTJul_FeVZ_k5fHgotXGQ/view?utm_content=DAFHfQnm6Y4&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink'>Resume</a>
+                </div>
             </div>
         </div>
     </div>
