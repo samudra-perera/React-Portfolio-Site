38b01d0eadfee6e110b392bd602fa426cdcbd313
Samudra Perera <samudrapup@gmail.com>
2022-09-02 15:23:04 -0400

    Added typing animation, added styling, need to do Media Queries

--- portfolio-site/src/components/landing/landing.jsx
+++ portfolio-site/src/components/landing/landing.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './landing.css'
 import landing from '../../img/landing.png'
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 const Landing = () => {
+    const {text} = useTypewriter({
+        words: [ " am a developer."," am a problem solver."," am a life long learner."," code.", " am a creative."],
+        loop: 0, 
+        typeSpeed: 150,
+        deleteSpeed: 50,
+        delaySpeed: 1000
+        })
     return (
         <div className="landing-container">
             <div className="landing-container-left">
                 <div className="landing-container-left-text">
-                    <h1 className="landing-title">Im a Full Stack Developer</h1>
-                    <p className="landing-blurb">I'm Samudra, thanks for checking my website out!</p>
+                    <h1 className="landing-title">I{text} <Cursor /></h1>
+                    <p className="landing-blurb">"I'm Samudra, thanks for checking my website out! To see some of my work click below."</p>
                     <button className="project-button">Projects</button>
                 </div>
             </div>
