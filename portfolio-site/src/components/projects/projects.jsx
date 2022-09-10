948c01243e1e2bb0e3bcfe29c8572cd9de6954a9
Samudra Perera <samudrapup@gmail.com>
2022-09-10 02:59:02 -0400

    finished styling the project Card

--- portfolio-site/src/components/projects/projects.jsx
+++ portfolio-site/src/components/projects/projects.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
             <div>
                 <h1 className="Projects-header">Projects</h1>
             </div>
-            <div>
+            <div className="card-container-main">
                 <ProjCard/>
             </div>
         </div>
@@ -17,7 +17,19 @@ const Projects = () => {
 const ProjCard = (props) => {
     return (
         <div className="card-container">
-            <h2>Yo</h2>
+            <span className="card-logo">Logo</span>
+            <h3 className="card-header">This is the title</h3>
+            <p className="card-para">This is the description of the project and no matter what Imma do what it takes to be the best</p>
+            <div className="card-footer">
+                <a className="card-github" href="google.com">
+                    <i class="fa-brands fa-github"></i>
+                    <span className="card-footer-span">Repo</span>
+                </a>
+                <a className="card-hosted-link" href="google.com">
+                    <i class="fa-solid fa-link"></i>
+                    <span className="card-footer-span">Link</span>
+                </a>
+            </div>
         </div>
     )
 }
