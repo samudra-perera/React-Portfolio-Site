044a4866f9883a1e2bb6226d7bf6c4fa6193e5cf
Samudra Perera <samudrapup@gmail.com>
2022-09-15 03:39:07 -0400

    added footer

--- portfolio-site/src/App.jsx
+++ portfolio-site/src/App.jsx
@@ -3,6 +3,7 @@ import Landing from './components/landing/landing';
 import AboutMe from "./components/about-me/about-me";
 import Projects from "./components/projects/projects";
 import Skills from "./components/my-skills/skills";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <AboutMe/>
         <Skills/>
         <Projects/>
+        <Footer/>
       </div>
   );
 }
---
+++ portfolio-site/src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer-container">
+            <p className="footer-text">Copyright © Samudra Perera. All rights reserved</p>
+        </div>
+    )
+}
+
+export default Footer
